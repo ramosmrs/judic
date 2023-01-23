c66566b3b3638eeb749663fd4311bf9091e402f0
Marcio <marcio.rsouza@gmail.com>
2023-01-23 15:21:28 -0300

    identação de código

--- pesquisa.php
+++ pesquisa.php
@@ -4,9 +4,9 @@
 
   $sql = "SELECT P.nrsei, P.numfunc, P.redmine, P.nome, F.nome AS ASSESSOR, S.descricao AS STATUS, A.descricao AS ASSUNTO, P.data_ini 
             FROM processo P, FUNCIONARIO F, STATUS S, ASSUNTO A 
-          WHERE P.assunto_id = A.id 
-            AND P.funcionario_id = F.id 
-            AND P.status_id = S.id";
+           WHERE P.assunto_id = A.id 
+             AND P.funcionario_id = F.id 
+             AND P.status_id = S.id";
 
   echo $sql;
   $conexao->query($sql);
